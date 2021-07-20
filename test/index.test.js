e2debab6c7cbf76a80573e8ab0984ec3e6e23ad0
Lahiru Wijewardana <lahiru.wijewardana@aeturnum.com>
2021-07-20 21:56:27 +0530

    Added debug, warn, trace logs with colors

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,19 @@ describe('Logger Test', async () => {
     LOGGER.info('This is a INFO log');
   });
 
-  it('Should print INFO log', async () => {
+  it('Should print DEBUG log', async () => {
+    LOGGER.debug('This is a DEBUG log');
+  });
+
+  it('Should print ERROR log', async () => {
     LOGGER.error('This is a ERROR log');
+  });
+
+  it('Should print WARN log', async () => {
+    LOGGER.warn('This is a WARN log');
+  });
+
+  it('Should print TRACE log', async () => {
+    LOGGER.traceLog('This is a TRACE log');
   });
 });
